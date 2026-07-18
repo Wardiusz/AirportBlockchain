0bb8a4aac7a7a2ea9a94a0e1227e72be02b5a77e
Wardiusz <lukaswardpl@gmail.com>
2026-07-18 19:06:27 +0200

    style: add space for readability

--- backend/src/main/java/com/airportblockchain/backend/service/FlightService.java
+++ backend/src/main/java/com/airportblockchain/backend/service/FlightService.java
@@ -6,9 +6,14 @@ import java.util.List;
 
 public interface FlightService {
     FlightData getFlight(String flightId) throws Exception;
+
     List<FlightData> getAllFlights() throws Exception;
+
     FlightData updateGate(String flightId, String newGate) throws Exception;
+
     FlightData createFlight(String flightId, String airline, String origin, String destination, String gate, String status, String scheduledDep) throws Exception;
+
     FlightData updateStatus(String flightId, String newStatus) throws Exception;
+
     String getFlightHistory(String flightId) throws Exception;
 }
