9ff619f55a659c12798c84b408cbc2901427f55f
Wardiusz <lukaswardpl@gmail.com>
2026-07-18 19:08:47 +0200

    style: remove space and public (in java 25 is not needed)

--- backend/src/main/java/com/airportblockchain/backend/AirportBackendApplication.java
+++ backend/src/main/java/com/airportblockchain/backend/AirportBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AirportBackendApplication {
-
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(AirportBackendApplication.class, args);
     }
-
 }
